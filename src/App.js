c4e265b63c3dd5eb07e0e1ff6f97c271a31122e6
gekaart <gekaarissta@gmail.com>
2022-06-20 12:26:50 +0700

    Answer Challenge 9

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Contact from "./components/Contact";
 
 // Uncomment untuk memuat daftar kontak
-// import contacts from './data/contacts.json';
+import contacts from "./data/contacts.json";
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
   return (
     <div className="App">
-      
+      <Header />
+      {contacts.map((item) => {
+        return <Contact data={item} />;
+        // console.log(item.name);
+      })}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,18 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+const Contact = ({ data }) => {
+  return (
+    <div className="Contact">
+      <img src={data.photo} alt="alt-foto" />
+      <div className="description">
+        <b>{data.name}</b>
+        <p>{data.phone}</p>
+        <p>{data.email}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
